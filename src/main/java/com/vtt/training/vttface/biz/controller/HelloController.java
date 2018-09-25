cc19a5b69e380c363be0d29d334fbd930226c2a5
vytuto <nguyenngoctho@hotmail.com>
2018-09-25 21:45:22 +0200

    Hello

--- src/main/java/com/vtt/training/vttface/biz/controller/HelloController.java
+++ src/main/java/com/vtt/training/vttface/biz/controller/HelloController.java
@@ -3,15 +3,14 @@ package com.vtt.training.vttface.biz.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/")
 public class HelloController {
 
-	@RequestMapping(method = RequestMethod.GET, produces = "application/json")
-	public ResponseEntity<String> loginOk(@RequestParam("access_token") String accesstoken) {
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<String> hello() {
 		return ResponseEntity.ok("Hello");
 	}
 }
